a2144a070a91b25d5f12e5e3ff44b256c863e521
Sunny Sun <newzealandsunny@gmail.com>
2021-10-27 15:21:11 -0400

    finish 23-31

---
+++ Section02_Diving_Into_Basics/App_Collection/23_Getting_Started_With_Styles.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderColor: 'black', borderWidth: 1, padding: 10}} />
+        <Button title="Add" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
